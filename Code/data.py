d6c731853338ee5cd89de2e42d59558f1df82842
saunack <kr.saunack@gmail.com>
2018-12-29 00:52:36 +0530

    Refactored code

--- Code/data.py
+++ Code/data.py
@@ -45,31 +45,37 @@ class Segmentation(Dataset):
 		return self.images
 
 def train(epochs=2,pad = 2):
-    dataset = Segmentation()
+    dataset = Segmentation().cuda()
     model = UNet(n_class = 1)
-    optim = torch.optim.SGD(model.parameters(),lr = 0.03, momentum = 0.8, weight_decay = 0.0005)
+    optimizer = torch.optim.SGD(model.parameters(),lr = 0.03, momentum = 0.8, weight_decay = 0.0005)
     loss_log = []
     criterion = nn.BCELoss()
 
     print("Starting training")
 
     for epoch in range(epochs):
-		    print("Starting Epoch #", epoch)
+        print("Starting Epoch #", epoch)
 
         train_loader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
         epoch_loss = 0
 
         for i,images in enumerate(train_loader):
+            # get the inputs
+            image, label = images['image'], images['segmented']
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
             ## Run the forward pass
-            outputs = model.forward(images['image'])
-
-            loss = criterion(outputs, images['segmented'])
+            outputs = model.forward(image).cuda() 
+            loss = criterion(outputs, label)
+            loss.backward()
+            
             loss_log.append(loss.item())
+            
             epoch_loss = epoch_loss + loss.item()
             
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
+            optimizer.step()
 
             print("Epoch #", epoch, "Batch #",i)
         
