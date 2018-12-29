52287830a3479e6079fc7bae90ff530c29be9b97
Kumar Saurav <mailkr.srv@gmail.com>
2018-12-29 11:03:05 +0530

    Corrected model

--- Code/model.py
+++ Code/model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torch.nn.init import normal_ as N
 import os.path as osp
 import torch
 
@@ -6,10 +7,9 @@ import torch
 # https://github.com/milesial/Pytorch-UNet/
 
 class UNet(nn.Module):
-    def __init__(self,n_class = 1,bilinear = False,pad=1):
+    def __init__(self, n_class=1, bilinear=False, pad=0):
         super(UNet,self).__init__()
         
-        self.drop = nn.Dropout2d(p=0.2)
         #DOWNSAMPLING
         #conv1
         self.dconv1_1 = nn.Conv2d(1,64,3,padding=pad)
@@ -31,6 +31,7 @@ class UNet(nn.Module):
         self.dconv3_2 = nn.Conv2d(256,256,3,padding=pad)
         self.drelu3_2 = nn.ReLU(inplace=True)
         self.dpool3 = nn.MaxPool2d(2,stride=2,ceil_mode=True)  #1/8
+        self.ddrop3 = nn.Dropout2d(p=0.2)
         
         #conv4
         self.dconv4_1 = nn.Conv2d(256,512,3,padding=pad)
@@ -38,6 +39,7 @@ class UNet(nn.Module):
         self.dconv4_2 = nn.Conv2d(512,512,3,padding=pad)
         self.drelu4_2 = nn.ReLU(inplace=True)
         self.dpool4 = nn.MaxPool2d(2,stride=2,ceil_mode=True)  #1/8
+        self.ddrop4 = nn.Dropout2d(p=0.2)
         
         #BOTTLENECK
         self.bconv1_1 = nn.Conv2d(512,1024,3,padding=pad)
@@ -50,46 +52,56 @@ class UNet(nn.Module):
         if bilinear:
             self.upool1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.upool1 = nn.ConvTranspose2d(1024,1024,3,stride=2,bias=False)
-        self.uconv1_1 = nn.Conv2d(1024+512,512,3,padding=pad)
+            self.upool1 = nn.ConvTranspose2d(1024,512,3,stride=2,bias=False)
+        self.uconv1_1 = nn.Conv2d(1024,512,3,padding=pad)
         self.urelu1_1 = nn.ReLU(inplace=True)
         self.uconv1_2 = nn.Conv2d(512,512,3,padding=pad)
         self.urelu1_2 = nn.ReLU(inplace=True)
+        self.udrop1 = nn.Dropout2d(p=0.2)
         
         #conv2
         if bilinear:
             self.upool2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.upool2 = nn.ConvTranspose2d(512,512,3,stride=2,bias=False)
-        self.uconv2_1 = nn.Conv2d(512+256,256,3,padding=pad)
+            self.upool2 = nn.ConvTranspose2d(512,256,3,stride=2,bias=False)
+        self.uconv2_1 = nn.Conv2d(512,256,3,padding=pad)
         self.urelu2_1 = nn.ReLU(inplace=True)
         self.uconv2_2 = nn.Conv2d(256,256,3,padding=pad)
         self.urelu2_2 = nn.ReLU(inplace=True)
+        self.udrop2 = nn.Dropout2d(p=0.2)
         
         #conv3
         if bilinear:
             self.upool3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.upool3 = nn.ConvTranspose2d(256,256,3,stride=2,bias=False)
-        self.uconv3_1 = nn.Conv2d(256+128,128,3,padding=pad)
+            self.upool3 = nn.ConvTranspose2d(256,128,3,stride=2,bias=False)
+        self.uconv3_1 = nn.Conv2d(256,128,3,padding=pad)
         self.urelu3_1 = nn.ReLU(inplace=True)
         self.uconv3_2 = nn.Conv2d(128,128,3,padding=pad)
         self.urelu3_2 = nn.ReLU(inplace=True)
+        self.udrop3 = nn.Dropout2d(p=0.2)
         
         #conv4
         if bilinear:
             self.upool4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.upool4 = nn.ConvTranspose2d(128,128,3,stride=2,bias=False)
-        self.uconv4_1 = nn.Conv2d(128+64,64,3,padding=pad)
+            self.upool4 = nn.ConvTranspose2d(128,64,3,stride=2,bias=False)
+        self.uconv4_1 = nn.Conv2d(128,64,3,padding=pad)
         self.urelu4_1 = nn.ReLU(inplace=True)
         self.uconv4_2 = nn.Conv2d(64,64,3,padding=pad)
         self.urelu4_2 = nn.ReLU(inplace=True)
+        self.udrop4 = nn.Dropout2d(p=0.2)
         
-        self.seg = nn.Conv2d(64,n_class,1,padding=0)
+        self.seg = nn.Conv2d(64,n_class,1,padding=pad)
         
+				self._init_weights()
 #        self._initialize_weights()
     
+		def _init_weights(self):
+				for m in self.modules():
+						if isinstance(m, nn.Conv2d):
+								nn.init.kaiming_normal_(m, nonlinearity='relu')
+
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -105,7 +117,7 @@ class UNet(nn.Module):
         #x1 has bigger dimensions than x2
         diffX = x1.size()[2] - x2.size()[2]
         diffY = x1.size()[3] - x2.size()[3]
-        x2 = nn.functional.pad(x2,(diffX//2,diffX-diffX//2,diffY//2,diffY-diffY//2))
+        x1 = nn.functional.pad(x1,(-diffX//2,-diffX+diffX//2,-diffY//2,-diffY+diffY//2))
         x2 = torch.cat([x2,x1],dim=1)
         return x2
     
@@ -125,13 +137,13 @@ class UNet(nn.Module):
         h = self.drelu3_1(self.dconv3_1(h))
         h = self.drelu3_2(self.dconv3_2(h))
         cc3 = h
-        h = self.drop(h)
+        h = self.ddrop3(h)
         h = self.dpool3(h)
         
         h = self.drelu4_1(self.dconv4_1(h))
         h = self.drelu4_2(self.dconv4_2(h))
         cc4 = h
-        h = self.drop(h)
+        h = self.ddrop4(h)
         h = self.dpool4(h)
         
         #BOTTLENECK
@@ -141,25 +153,25 @@ class UNet(nn.Module):
         #UPSAMPLING
         h = self.upool1(h)
         h = self.concat(h,cc4)
-        h = self.drop(h)
+        h = self.udrop1(h)
         h = self.urelu1_1(self.uconv1_1(h))
         h = self.urelu1_2(self.uconv1_2(h))
         
         h = self.upool2(h)
         h = self.concat(h,cc3)
-        h = self.drop(h)
+        h = self.udrop2(h)
         h = self.urelu2_1(self.uconv2_1(h))
         h = self.urelu2_2(self.uconv2_2(h))
         
         h = self.upool3(h)
         h = self.concat(h,cc2)
-        h = self.drop(h)
+        h = self.udrop3(h)
         h = self.urelu3_1(self.uconv3_1(h))
         h = self.urelu3_2(self.uconv3_2(h))
         
         h = self.upool4(h)
         h = self.concat(h,cc1)
-        h = self.drop(h)
+        h = self.udrop4(h)
         h = self.urelu4_1(self.uconv4_1(h))
         h = self.urelu4_2(self.uconv4_2(h))
         
