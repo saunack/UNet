d02afdace4efa87470c17f092ec9e4c756204a90
Kumar Saurav <mailkr.srv@gmail.com>
2018-12-29 11:02:44 +0530

    Corrected error metric

--- Code/train.py
+++ Code/train.py
@@ -1,21 +1,20 @@
 import torch
 from torch.utils.data import DataLoader
+from torch.nn import functional as F
 from model import UNet
 from dataset import Segmentation
 
-def train(epochs=2,pad = 2):
+def train(epochs=2, pad=2):
     dataset = Segmentation()
     model = UNet(n_class = 1).cuda() if torch.cuda.is_available() else UNet(n_class = 1)
-    optimizer = torch.optim.SGD(model.parameters(),lr = 0.03, momentum = 0.33, weight_decay = 0.0005)
+    optimizer = torch.optim.SGD(model.parameters(), lr = 0.03, momentum = 0.99, weight_decay = 0.0005)
     loss_log = []
-    criterion = torch.nn.BCEWithLogitsLoss()
-
-    print("Starting training")
+    criterion = torch.nn.CrossEntropyLoss(reduction='sum')
 
     for epoch in range(epochs):
-        print("Starting Epoch #", epoch)
+        print("Starting Epoch #{}".format(epoch))
 
-        train_loader = DataLoader(dataset=dataset, batch_size=3, shuffle=True)
+        train_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
         epoch_loss = 0
 
         for i,images in enumerate(train_loader):
@@ -36,9 +35,9 @@ def train(epochs=2,pad = 2):
             
             optimizer.step()
 
-            print("Epoch #", epoch, "Batch #", i, " Loss: ", loss.item())
+            print("Epoch #{} Batch #{} Loss: {}".format(epoch,i,loss.item()))
         
-        print("Epoch ",epoch," finished. Loss : ",loss)
+        print("Epoch",epoch," finished. Loss :",loss)
         epoch_loss = 0
         
 train(6)
