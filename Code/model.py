2399046802645bca4980ddf9d27eca0c4fe604f5
saunack <kr.saunack@gmail.com>
2018-12-30 18:42:15 +0530

    changed to LeakyReLU

--- Code/model.py
+++ Code/model.py
@@ -13,39 +13,39 @@ class UNet(nn.Module):
         #DOWNSAMPLING
         #conv1
         self.dconv1_1 = nn.Conv2d(1,64,3,padding=pad)
-        self.drelu1_1 = nn.ReLU()
+        self.drelu1_1 = nn.LeakyReLU()
         self.dconv1_2 = nn.Conv2d(64,64,3,padding=pad)
-        self.drelu1_2 = nn.ReLU()
+        self.drelu1_2 = nn.LeakyReLU()
         self.dpool1 = nn.MaxPool2d(2,stride=2)  #1/2
         
         #conv2
         self.dconv2_1 = nn.Conv2d(64,128,3,padding=pad)
-        self.drelu2_1 = nn.ReLU()
+        self.drelu2_1 = nn.LeakyReLU()
         self.dconv2_2 = nn.Conv2d(128,128,3,padding=pad)
-        self.drelu2_2 = nn.ReLU()
+        self.drelu2_2 = nn.LeakyReLU()
         self.dpool2 = nn.MaxPool2d(2,stride=2)  #1/4
         
         #conv3
         self.dconv3_1 = nn.Conv2d(128,256,3,padding=pad)
-        self.drelu3_1 = nn.ReLU()
+        self.drelu3_1 = nn.LeakyReLU()
         self.dconv3_2 = nn.Conv2d(256,256,3,padding=pad)
-        self.drelu3_2 = nn.ReLU()
+        self.drelu3_2 = nn.LeakyReLU()
         self.dpool3 = nn.MaxPool2d(2,stride=2)  #1/8
         self.ddrop3 = nn.Dropout2d(p=0.2)
         
         #conv4
         self.dconv4_1 = nn.Conv2d(256,512,3,padding=pad)
-        self.drelu4_1 = nn.ReLU()
+        self.drelu4_1 = nn.LeakyReLU()
         self.dconv4_2 = nn.Conv2d(512,512,3,padding=pad)
-        self.drelu4_2 = nn.ReLU()
+        self.drelu4_2 = nn.LeakyReLU()
         self.dpool4 = nn.MaxPool2d(2,stride=2)  #1/8
         self.ddrop4 = nn.Dropout2d(p=0.2)
         
         #BOTTLENECK
         self.bconv1_1 = nn.Conv2d(512,1024,3,padding=pad)
-        self.brelu1_1 = nn.ReLU()
+        self.brelu1_1 = nn.LeakyReLU()
         self.bconv1_2 = nn.Conv2d(1024,1024,3,padding=pad)
-        self.brelu1_2 = nn.ReLU()
+        self.brelu1_2 = nn.LeakyReLU()
         
         #UPSAMPLING
         #conv1
@@ -54,9 +54,9 @@ class UNet(nn.Module):
         else:
             self.upool1 = nn.ConvTranspose2d(1024,512,2,stride=2)
         self.uconv1_1 = nn.Conv2d(1024,512,3,padding=pad)
-        self.urelu1_1 = nn.ReLU()
+        self.urelu1_1 = nn.LeakyReLU()
         self.uconv1_2 = nn.Conv2d(512,512,3,padding=pad)
-        self.urelu1_2 = nn.ReLU()
+        self.urelu1_2 = nn.LeakyReLU()
         self.udrop1 = nn.Dropout2d(p=0.2)
         
         #conv2
@@ -65,9 +65,9 @@ class UNet(nn.Module):
         else:
             self.upool2 = nn.ConvTranspose2d(512,256,2,stride=2)
         self.uconv2_1 = nn.Conv2d(512,256,3,padding=pad)
-        self.urelu2_1 = nn.ReLU()
+        self.urelu2_1 = nn.LeakyReLU()
         self.uconv2_2 = nn.Conv2d(256,256,3,padding=pad)
-        self.urelu2_2 = nn.ReLU()
+        self.urelu2_2 = nn.LeakyReLU()
         self.udrop2 = nn.Dropout2d(p=0.2)
         
         #conv3
@@ -76,9 +76,9 @@ class UNet(nn.Module):
         else:
             self.upool3 = nn.ConvTranspose2d(256,128,2,stride=2)
         self.uconv3_1 = nn.Conv2d(256,128,3,padding=pad)
-        self.urelu3_1 = nn.ReLU()
+        self.urelu3_1 = nn.LeakyReLU()
         self.uconv3_2 = nn.Conv2d(128,128,3,padding=pad)
-        self.urelu3_2 = nn.ReLU()
+        self.urelu3_2 = nn.LeakyReLU()
         self.udrop3 = nn.Dropout2d(p=0.2)
         
         #conv4
@@ -87,9 +87,9 @@ class UNet(nn.Module):
         else:
             self.upool4 = nn.ConvTranspose2d(128,64,2,stride=2)
         self.uconv4_1 = nn.Conv2d(128,64,3,padding=pad)
-        self.urelu4_1 = nn.ReLU()
+        self.urelu4_1 = nn.LeakyReLU()
         self.uconv4_2 = nn.Conv2d(64,64,3,padding=pad)
-        self.urelu4_2 = nn.ReLU()
+        self.urelu4_2 = nn.LeakyReLU()
         self.udrop4 = nn.Dropout2d(p=0.2)
         
         self.seg = nn.Conv2d(64,n_class,1,padding=pad)
